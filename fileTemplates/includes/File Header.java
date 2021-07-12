9c02cea7ae1961ca5f59b27546d7fe04ccbe866b
wangweixu <kcxh8831658.com>
2021-07-12 09:44:30 +0800

    AI-4.2.1 <wangweixu@bogon Create baseRefactoring.xml, ide.general.xml, project.default.xml, lombok-plugin.xml, noteGroup.xml, yiiguxing.translation.xml, gradle.experimental.xml, Default.xml, designSurface.xml, visualizationTool.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, File Header.java, gradle.settings.xml, filetypes.xml, AndroidComments.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, path.macros.xml, templates.xml, jdk.table.xml, IntelliJ IDEA Classic copy.xml, lightEdit.xml, laf.xml, vcs.xml, editor.codeinsight.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,11 @@
+/**
+ * <pre>
+ *      author:         ${USER}
+ *      date:           ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+ *      description:    ${description}
+ *      upUser:         
+ *      upDate:         
+ *      upRemark:       更新说明:
+ *      version:        v1.0
+ * </pre>
+ */
